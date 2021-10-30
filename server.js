5776d47a120eb63d528df415a94d22d93c6b516d
Ken Ilo <keneilo91@yahoo.com>
2021-10-29 20:37:31 -0700

    tested functionality for inquirer

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+// npm packages and modules
 const inquirer = require('inquirer');
+
+
+inquirer.prompt([
+    {
+        type: "list",
+        name: "roles",
+        message: "What would you like to do ?",
+        choices: ['View All Employees', 'View All Roles', 'View All Departments']
+    },
+])
+
+.then(({roles}) => {
+    if (roles === "View All Employees") {
+        
+    }
+    
+});
