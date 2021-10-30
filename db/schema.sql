04d2d9ba571149160b5b4b8e5751b54e1cb9bbb7
Ken Ilo <keneilo91@yahoo.com>
2021-10-29 21:06:49 -0700

    created database folder and files

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    job_titles VARCHAR(30) NOT NULL,
+    department VARCHAR(30) NOT NULL,
+    salary VARCHAR(30) NOT NULL,
+    manager VARCHAR(30) NOT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO employees (first_name, last_name, job_titles, department, salary, manager)
+VALUES
+    ('Ken', 'Ilo', 'sales lead', 'sales', 20000, 'Ashley Rodriquez'),
+    ('Joe', 'Attah', 'marketer', 'engineering', 30000, 'James Morrison');
